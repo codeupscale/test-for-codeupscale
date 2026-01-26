c8f8e2990a159b002e13c0b5c406f403792c778c
Umer Maalik <umer@Umers-MacBook-Air.local>
2026-01-26 22:37:37 +0500

    task completed

--- client/src/components/ShopBy.jsx
+++ client/src/components/ShopBy.jsx
@@ -1,12 +1,18 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import HorSlider from "./HorSlider";
+import { Brands } from "./GenInfo";
 
 const ShopBy = ({ filter, title }) => {
   const [products, setProducts] = useState([]);
   const [error, setError] = useState(null);
   const [loading, setLoading] = useState(true);
-
+  const data = [
+    { src: "/GenInfo/adidas.jpg", name: "Adidas", to: "/search/adidas" },
+    { src: "/GenInfo/nike.png", name: "Nike", to: "/search/nike" },
+    { src: "/GenInfo/skechers.jpg", name: "Skechers", to: "/search/skechers" },
+    { src: "/GenInfo/puma.jpg", name: "Puma", to: "/search/puma" },
+  ];
   useEffect(() => {
     let isMounted = true;
     const fetchData = async () => {
@@ -49,6 +55,8 @@ const ShopBy = ({ filter, title }) => {
               home={true}
             />
           ))}
+                <Brands title="Best Sellers" />
+          
         </div>
       </div>
     </>
